def8afbad8bc1148a0c31b11df3644b973bdd19c
Mohammad Shariq Pir <pirshariq419@gmail.com>
2025-06-29 20:15:55 +0530

    Update prog3 PM.py

--- prog3 PM.py
+++ prog3 PM.py
@@ -1,28 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+data = {
+    'Name': ['Aman', 'Bina', 'Chetan'],
+    'Marks': [80, 90, 85]
+}
 
-print(df.head())
-print(df.describe())
-print(df['species'].value_counts())
+df = pd.DataFrame(data)
+print(df)
 
-df['sepal_length'].hist(bins=20, color='skyblue', edgecolor='black')
-plt.title('Sepal Length Distribution')
-plt.xlabel('Sepal Length (cm)')
-plt.ylabel('Frequency')
-plt.show()
-
-colors = {'setosa':'red', 'versicolor':'green', 'virginica':'blue'}
-plt.scatter(df['sepal_length'], df['sepal_width'], c=df['species'].map(colors))
-plt.title('Sepal Length vs Width')
-plt.xlabel('Sepal Length (cm)')
-plt.ylabel('Sepal Width (cm)')
-plt.show()
-
-df.boxplot(column='petal_length', by='species', grid=False)
-plt.title('Petal Length by Species')
-plt.suptitle('')
-plt.xlabel('Species')
-plt.ylabel('Petal Length (cm)')
+plt.bar(df['Name'], df['Marks'], color='skyblue')
+plt.title('Student Marks')
+plt.xlabel('Name')
+plt.ylabel('Marks')
 plt.show()
